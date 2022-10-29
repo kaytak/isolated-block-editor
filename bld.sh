806aa6617b203ab5fc4c52186ef3d9d42cc916d6
Keita <kei.kand@live.jp>
2022-10-29 18:33:12 +0200

    bld

--- bld.sh
+++ bld.sh
@@ -1,11 +1,11 @@
-#! /usr/bin/tsh
+#! C:\Users\kandak\AppData\Local\Programs\Git\usr\bin\bash.exe
 
 BUILD_ENV=es6
-babel src --out-dir build-module --source-maps --ignore 'src/**/__tests__/*.js','src/browser/*' --copy-files --no-copy-ignored && rm -rf build-module/browser
+./node_modules/.bin/babel src --out-dir build-module --source-maps --ignore 'src/**/__tests__/*.js','src/browser/*' --copy-files --no-copy-ignored && rm -rf build-module/browser
 BUILD_ENV=cjs
-babel src --out-dir build --source-maps --ignore 'src/**/__tests__/*.js','src/browser/*' --copy-files --no-copy-ignored && rm -rf build/browser
+./node_modules/.bin/babel src --out-dir build --source-maps --ignore 'src/**/__tests__/*.js','src/browser/*' --copy-files --no-copy-ignored && rm -rf build/browser
 BUILD_ENV=es6
 NODE_ENV=production
-webpack --mode production --progress --config ./webpack.config.browser.js && rm build-browser/core.js
-tsc --build
+./node_modules/.bin/webpack --mode production --progress --config ./webpack.config.browser.js && rm build-browser/core.js
+./node_modules/.bin/tsc --build
 
