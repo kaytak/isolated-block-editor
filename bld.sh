d430928c6f9db1af302e091374bc1be267df80d1
Keita <kei.kand@live.jp>
2022-10-31 19:43:38 +0100

    bld

--- bld.sh
+++ bld.sh
@@ -3,12 +3,12 @@
 BUILD_ENV=es6
 #./node_modules/.bin/babel src --out-dir build-module --source-maps --ignore 'src/**/__tests__/*.js','src/browser/*' --copy-files --no-copy-ignored && rm -rf build-module/browser
 yarn build:es6a
-BUILD_ENV=cjs
-yarn build:cjsa
+#BUILD_ENV=cjs
+#yarn build:cjsa
 #./node_modules/.bin/babel src --out-dir build --source-maps --ignore 'src/**/__tests__/*.js','src/browser/*' --copy-files --no-copy-ignored && rm -rf build/browser
 BUILD_ENV=es6
 NODE_ENV=production
 yarn build:browsera
 #./node_modules/.bin/webpack --mode production --progress --config ./webpack.config.browser.js && rm build-browser/core.js
 #./node_modules/.bin/tsc --build
-yarn build:types
+#yarn build:types
